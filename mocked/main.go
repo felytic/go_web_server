6aad61f4c5041f0cbe39544b7ac99f35c4fe67d4
felytic <felytic@gmail.com>
2020-11-03 14:25:49 +0200

    Mocked services for all payment systems

--- mocked/main.go
+++ mocked/main.go
@@ -8,7 +8,7 @@ import (
 	"net/http"
 )
 
-const port = "8090"
+const failurePercentage = 15
 
 type UrlResponse struct {
 	Url  string `json:"url"`
@@ -23,32 +23,53 @@ func GetIP(r *http.Request) string {
 	return r.RemoteAddr
 }
 
-func appleHandler(writer http.ResponseWriter, request *http.Request) {
-	appleResponse := UrlResponse{
-		Url:  "https://apple.com",
-		Name: "Apple Pay",
+type handlerFuncType func(http.ResponseWriter, *http.Request)
+
+func payResponseHandler(name string, url string) handlerFuncType {
+	payResponse := UrlResponse{
+		Url:  url,
+		Name: name,
 	}
 
-	respJSON, err := json.Marshal(appleResponse)
+	respJSON, err := json.Marshal(payResponse)
 
-	if err != nil {
-		log.Print("[ERROR]", err)
-		fmt.Fprintln(writer, err)
+	return func(writer http.ResponseWriter, request *http.Request) {
+		if err != nil {
+			log.Print("[ERROR]", err)
+			fmt.Fprintln(writer, err)
+		}
+
+		// throw 500 error with 15% probability
+		if rand.Intn(100) < failurePercentage {
+			log.Print("[", GetIP(request), "] 500 Internal Server Error")
+			writer.WriteHeader(http.StatusInternalServerError)
+		} else {
+			log.Print("[", GetIP(request), "] 200 OK")
+			writer.Header().Set("Content-Type", "application/json")
+			writer.Write(respJSON)
+		}
 	}
 
-	// throw 500 error with 30% probability
-	if rand.Intn(10) < 3 {
-		log.Print("[", GetIP(request), "] 500 Internal Server Error")
-		writer.WriteHeader(http.StatusInternalServerError)
-	} else {
-		log.Print("[", GetIP(request), "] 200 OK")
-		writer.Header().Set("Content-Type", "application/json")
-		writer.Write(respJSON)
+}
+
+func runPaymentServer(port string, name string, url string) {
+	mux := http.NewServeMux()
+	handler := payResponseHandler(name, url)
+	mux.HandleFunc("/", handler)
+
+	addr := ":" + port
+	server := http.Server{
+		Addr:    addr,
+		Handler: mux,
 	}
+	log.Print("Started ", name, "server at port ", port)
+	log.Fatal(server.ListenAndServe())
+
 }
 
 func main() {
-	http.HandleFunc("/", appleHandler)
-	log.Print("Started server at port ", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	go runPaymentServer("8090", "Apple Pay", "https://apple.com")
+	go runPaymentServer("8091", "Google Pay", "https://google.com")
+	go runPaymentServer("8092", "Pay Pal", "https://paypal.com")
+	runPaymentServer("8093", "Stripe", "https://stripe.com")
 }
